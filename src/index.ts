7b702d6d3834800aee01465ed6f800a0038c0b38
benily <leekwokhinben@gmail.com>
2021-11-29 22:23:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import { Browser, ElementHandle, Page } from "puppeteer";
+
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+	const browser: Browser = await puppeteer.launch({ headless: false });
+	const page: Page = await browser.newPage();
+	await page.goto("https://toggl.com/jobs/", {
+		waitUntil: "networkidle0",
+	});
+
+	const links = await page.$$eval("a", (anchors: Element[]) => {
+		return anchors.map((a) => a);
+	});
+	console.log(links);
+
+	await browser.close();
+})();
