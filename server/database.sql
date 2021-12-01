8168c5b3d56d13b0ee68a52b0ab8dd78833ef841
benily <leekwokhinben@gmail.com>
2021-12-01 18:33:57 +0800

    update database settings

--- server/database.sql
+++ server/database.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE jobs;
 
+CREATE EXTENSION IF NOT EXIST "uuid-ossp"
+
 CREATE TABLE job(
-	job_id SERIAL PRIMARY KEY,
-	company_name VARCHAR(255),
-	job_title VARCHAR(255)
+	job_id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+	job_title VARCHAR(255) NOT NULL,
+	company_name VARCHAR(255) NOT NULL,
+	location VARCHAR(255) NOT NULL,
+	job_link TEXT NOT NULL,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
 );
