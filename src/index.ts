5daf5d1c8e82a3908ea3c5e29dc1eb6aa45d1d7a
Lee Kwok Hin <leekwokhinben@gmail.com>
2022-10-10 23:19:35 +0800

    Added express.js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log('hello');
-console.log('gg');
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+	// eslint-disable-next-line no-console
+	console.log(`Express is listening on port ${port}`);
+});
