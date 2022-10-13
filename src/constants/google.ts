d2d2bfb9c479c85f36c75683d04d22f05d500353
benilyxdd <benilyxdd@gmail.com>
2022-10-14 00:47:05 +0800

    Added constants for google

--- src/constants/google.ts
+++ src/constants/google.ts
@@ -1,1 +1,5 @@
-export const baseURL = 'https://careers.google.com/jobs/results/';
+export const baseURL = "https://careers.google.com/jobs/results/";
+
+// Selector in Google Career Page
+export const pageNumberSelector =
+	"#jump-content > div.gc-wrapper > div > div.wrapper__maincol > main > div > div:nth-child(2) > div > div > p";
