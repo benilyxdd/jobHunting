82bfe2a285dc204c4b52f8b08d1374c02facd13a
benilyxdd <benilyxdd@gmail.com>
2022-10-14 00:17:30 +0800

    combined tests on Google Utils

--- src/utils/google/index.test.ts
+++ src/utils/google/index.test.ts
@@ -7,18 +7,16 @@ describe("Google Utils", () => {
 	describe("Function", () => {
 		it("encodeSearchURL", () => {
 			const URL = "https://google.com/";
+
+			// test 1
 			const searchParams = { query: "hello" };
 			const encodedURL = encodeSearchURL(URL, searchParams);
-
 			expect(encodedURL).to.deep.equal("https://google.com/?q=hello");
-		});
 
-		it("encodeSearchURL with special char", () => {
-			const URL = "https://google.com/";
-			const searchParam = { query: "search with space and !@#$%^&*(" };
-			const encodedURL = encodeSearchURL(URL, searchParam);
-
-			expect(encodedURL).to.deep.equal(
+			// test 2
+			const searchParam2 = { query: "search with space and !@#$%^&*(" };
+			const encodedURL2 = encodeSearchURL(URL, searchParam2);
+			expect(encodedURL2).to.deep.equal(
 				"https://google.com/?q=search%20with%20space%20and%20%21%40%23%24%25%5E%26%2A%28"
 			);
 		});
