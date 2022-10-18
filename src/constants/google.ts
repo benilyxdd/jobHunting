f480b91f6228dfec057e8ca965417215809c7241
benilyxdd <benilyxdd@gmail.com>
2022-10-18 14:21:45 +0800

    Added job details selector

--- src/constants/google.ts
+++ src/constants/google.ts
@@ -4,3 +4,19 @@ export const baseURL = "https://careers.google.com/jobs/results/";
 export const pageNumberSelector =
 	"#jump-content > div.gc-wrapper > div > div.wrapper__maincol > main > div > div:nth-child(2) > div > div > p";
 export const jobContainerSelector = ".gc-card";
+
+// job details selector
+export const jobSelector = {
+	addressCountry:
+		'[itemprop="jobLocation"] > [itemprop="address"] > [itemprop="addressCountry"]', // HTMLSpanElement
+	addressLocality:
+		'[itemprop="jobLocation"] > [itemprop="address"] > [itemprop="addressLocality"]', //HTMLSpanElement
+	addressRegion:
+		'[itemprop="jobLocation"] > [itemprop="address"] > [itemprop="addressRegion"]', // HTMLSpanElement
+	applyLink: '[aria-label="Apply for this job (Opens in a new window)"]', // HTMLAnchorElement
+	datePosted: '[itemprop="datePosted"]', // HTMLMetaElement
+	description: '[class="gc-user-generated-content"][itemprop="description"]', // HTMLDivElement
+	qualifications: '[itemprop="qualifications"]', // HTMLDivElement
+	responsibilities: '[itemprop="responsibilities"]', // HTMLDivElement
+	title: '[itemprop="title"]', // HTMLHeadingElement (h1)
+};
